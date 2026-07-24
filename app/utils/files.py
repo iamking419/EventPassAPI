e915697b5d1b70b03fdf5e9132cd1bc8253730e8
K I N G <michaelkm555@gmail.com>
2026-07-24 20:33:42 +0100

    Update files.py

--- app/utils/files.py
+++ app/utils/files.py
@@ -5,13 +5,14 @@ UPLOAD_DIR = "uploads"
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-
 def save_image(file):
-    ext = file.filename.split(".")[-1]
+    ext = file.filename.split(".")[-1].lower()
     filename = f"{uuid4()}.{ext}"
-    path = os.path.join(UPLOAD_DIR, filename)
 
-    with open(path, "wb") as buffer:
+    filepath = os.path.join(UPLOAD_DIR, filename)
+
+    with open(filepath, "wb") as buffer:
         buffer.write(file.file.read())
 
-    return path
+    # Return the web-accessible path
+    return f"/uploads/{filename}"
